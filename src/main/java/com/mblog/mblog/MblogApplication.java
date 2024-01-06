7693b88de3acb06ec525a4b95b8565b6ce97ecde
ADARSHGUPTA114 <86097862+ADARSHgupta114@users.noreply.github.com>
2024-01-06 17:52:49 +0530

    mblog project started

---
+++ src/main/java/com/mblog/mblog/MblogApplication.java
@@ -0,0 +1,13 @@
+package com.mblog.mblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MblogApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/mblog/mblog/entites/Post.java
@@ -0,0 +1,21 @@
+package com.mblog.mblog.entites;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="posts")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private String despcription;
+    private String content;
+
+}
